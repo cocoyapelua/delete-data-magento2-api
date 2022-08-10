d26896e9549b8643df894f51c87520926ccb84b7
Juan Santana <jjhurtado07@gmail.com>
2022-08-10 14:09:46 -0400

    Adding condition for product-category

--- conf/constants.py
+++ conf/constants.py
@@ -8,12 +8,15 @@ AUTH = os.environ.get("AUTH", None)
 ACTION = os.environ.get("ACTION", None)
 COL = ""
 API_URL = ""
+CATEGORY_ID = os.environ.get("CATEGORY_ID", None)
 
 if ACTION == 'product':
     COL = 'sku'
     API_URL = 'rest/V1/products'
-
 elif ACTION == 'address':
     COL = 'entity_id'
     API_URL = 'rest/V1/addresses'
+elif ACTION == 'product-category':
+    COL = 'sku'
+    API_URL = f'rest/V1/categories/{CATEGORY_ID}/products'
 
